f1a63a647248dabbb91ea9051366dcf9e4d7fd44
c22beche <capucine.bechemin@imt-atlantique.net>
2022-10-11 15:37:35 +0200

    Doc_MovieOK

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # UE-AD-A1-REST
+**Esteban GOMEZ et Capucine BECHEMIN**
 
-test
+_Ce TP modelise une gestion de séances de cinéma._
+
+TP VERT : terminé
+
+TP BLEU : terminé
+
+TP ROUGE : x
+
+### Mise en place
+* Clonez le repository
+* Ouvrez ce dossier dans votre IDE
+* Pour chacun des services ouvrez un terminal à la racine du TP 
+* Entrez les commandes suivantes dans chacun d'eux
+
+`cd .\movie\ ` `py .\movie.py`
+
+
+`cd .\booking\ ` `py .\booking.py`
+
+
+`cd .\showtime\ ` `py .\showtime.py`
+
+
+`cd .\user\ ` `py .\user.py`
+
+
+### Documentation des routes des APIs
+* Movie
+`http://172.16.128.224:3200/help`
--- movie/movie.py
+++ movie/movie.py
@@ -9,92 +9,145 @@ PORT = 3200
 HOST = '0.0.0.0'
 
 with open('{}/databases/movies.json'.format("."), "r") as jsf:
-   movies = json.load(jsf)["movies"]
+    movies = json.load(jsf)["movies"]
 
-# root message
+
+# Welcome route : GET : http://172.16.128.224:3200/
 @app.route("/", methods=['GET'])
 def home():
-    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
+    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>", 200)
 
+# Help route : GET : http://172.16.128.224:3200/help
+@app.route("/help", methods=['GET'])
+def help():
+    return make_response(render_template('help.html', body_text='This is my HTML help documentation for Movie service'), 200)
+
+
+# Template route : GET : http://172.16.128.224:3200/template
 @app.route("/template", methods=['GET'])
 def template():
-    return make_response(render_template('index.html', body_text='This is my HTML template for Movie service'),200)
+    return make_response(render_template('index.html', body_text='This is my HTML template for Movie service'), 200)
 
+
+# Get all json database route : GET : http://172.16.128.224:3200/json
 @app.route("/json", methods=['GET'])
 def get_json():
     res = make_response(jsonify(movies), 200)
     return res
 
-@app.route("/movies/<movieid>", methods=['GET'])
-def get_movie_byid(movieid):
-    for movie in movies:
-        if str(movie["id"]) == str(movieid):
-            res = make_response(jsonify(movie),200)
-            return res
-    return make_response(jsonify({"error":"Movie ID not found, you should create a movie by '/movies/<movieid>' post route."}),400)
 
-@app.route("/moviesbytitle", methods=['GET'])
-def get_movie_bytitle():
-    json = ""
+# Get movie by id route : GET : http://172.16.128.224:3200/movies/720d006c-3a57-4b6a-b18f-9b713b073f3c
+@app.route("/movies/<movie_id>", methods=['GET'])
+def get_movie_byid(movie_id):
+    for movie in movies:
+        if str(movie["id"]) == str(movie_id):
+            res = make_response(jsonify(movie), 200)
+            return res
+    return make_response(
+        jsonify({"error": "Movie ID not found, you should create a movie by '/movies/<movie_id>' post route."}), 400)
+
+
+# Get movie by id route
+# ↓ Postman
+# GET : http://172.16.128.224:3200/movies_by_title
+# Key : title
+# Value : The Good Dinosaur
+@app.route("/movies_by_title", methods=['GET'])
+def get_movie_by_title():
+    jsonRes = ""
     if request.args:
         req = request.args
         for movie in movies:
             if str(movie["title"]) == str(req["title"]):
-                json = movie
-
-    if not json:
-        res = make_response(jsonify({"error": "movie title not found, you should create a movie by '/movies/<movieid>' post route"}), 400)
+                jsonRes = movie
+    if not jsonRes:
+        res = make_response(jsonify({"error": "movie title not found, you should create a movie by "
+                                              "'/movies/<movie_id>' post route"}), 400)
     else:
-        res = make_response(jsonify(json),200)
+        res = make_response(jsonify(jsonRes), 200)
     return res
 
-@app.route("/moviesbyrate", methods=['GET'])
-def get_movie_byrate():
-    json = []
+
+# Get movie by rate route
+# ↓ Postman
+# GET : http://172.16.128.224:3200/movies_by_rate
+# Key : rating
+# Value : 7.4
+@app.route("/movies_by_rate", methods=['GET'])
+def get_movie_by_rate():
+    jsonRes = []
     if request.args:
         req = request.args
         for movie in movies:
             if float(movie["rating"]) == float(req["rating"]):
-                json.append(movie)
-
-    if not json:
-        res = make_response(jsonify({"error":"movie with this rate not found, you should check movies by '/json' get route"}),400)
+                jsonRes.append(movie)
+    if not jsonRes:
+        res = make_response(
+            jsonify({"error": "movie with this rate not found, you should check movies by '/json' get route"}), 400)
     else:
-        res = make_response(jsonify(json),200)
+        res = make_response(jsonify(jsonRes), 200)
     return res
 
-@app.route("/movies/<movieid>", methods=['POST'])
-def create_movie(movieid):
+
+# Create movie by id route
+# ↓ Postman
+# POST : http://172.16.128.224:3200/movies/1
+# Body Raw Json :
+"""
+    {
+        "director": "Antoine",
+        "id": "1",
+        "rating": 7.4,
+        "title": "ArchiDistribute"
+    }
+"""
+@app.route("/movies/<movie_id>", methods=['POST'])
+def create_movie(movie_id):
     req = request.get_json()
     for movie in movies:
-        if str(movie["id"]) == str(movieid):
-            return make_response(jsonify({"error":"an existing item already exists"}),409)
+        if str(movie["id"]) == str(movie_id):
+            return make_response(jsonify({"error": "an existing item already exists"}), 409)
     movies.append(req)
-    res = make_response(jsonify(req),200)
+    res = make_response(jsonify(req), 200)
     return res
 
-@app.route("/movies/<movieid>/<rate>", methods=['PUT'])
-def update_movie_rating(movieid, rate):
+
+# Change movie rate by id route
+# ↓ Postman
+# PUT : http://172.16.128.224:3200/movies/1/5.2
+# Body Raw Json :
+"""
+    {
+        "director": "Antoine",
+        "id": "1",
+        "rating": 5.2,
+        "title": "ArchiDistribute"
+    }
+"""
+@app.route("/movies/<movie_id>/<rate>", methods=['PUT'])
+def update_movie_rating(movie_id, rate):
     for movie in movies:
-        if str(movie["id"]) == str(movieid):
+        if str(movie["id"]) == str(movie_id):
             movie["rating"] = float(rate)
-            res = make_response(jsonify(movie),200)
+            res = make_response(jsonify(movie), 200)
             return res
-
-    res = make_response(jsonify({"error":"movie ID not found, you should check movies by '/json' get route"}),200)
+    res = make_response(jsonify({"error": "movie ID not found, you should check movies by '/json' get route"}), 400)
     return res
 
-@app.route("/movies/<movieid>", methods=['DELETE'])
-def delete_movie(movieid):
+
+# Delete movie by id route
+# ↓ Postman
+# DELETE : http://172.16.128.224:3200/movies/1
+@app.route("/movies/<movie_id>", methods=['DELETE'])
+def delete_movie(movie_id):
     for movie in movies:
-        if str(movie["id"]) == str(movieid):
+        if str(movie["id"]) == str(movie_id):
             movies.remove(movie)
-            return make_response(jsonify(movie),200)
-
-    res = make_response(jsonify({"error":"bad input parameter"}),400)
+            return make_response(jsonify(movie), 200)
+    res = make_response(jsonify({"error": "movie ID not found, you should check movies by '/json' get route"}), 400)
     return res
+
 
 if __name__ == "__main__":
-    #p = sys.argv[1]
-    print("Server running in port %s"%(PORT))
+    print("Server running in port %s" % PORT)
     app.run(host=HOST, port=PORT)
