6b1b3b2abb049096664857a2dbcb8799a4f43eea
EstebanGomez <gomez.esteban@outlook.fr>
2022-10-11 16:05:42 +0200

    VERT_User Comments

--- user/user.py
+++ user/user.py
@@ -10,21 +10,30 @@ HOST = '0.0.0.0'
 
 with open('{}/databases/users.json'.format("."), "r") as jsf:
     users = json.load(jsf)["users"]
-@app.route("/documentation", methods=['GET'])
-def user_documentation(userid):
-    return make_response(jsonify({"Error": "user not exist"}), 400)
-@app.route("/bookingsbyuserid/<userid>", methods=['GET'])
-def bookingsbyuserid(userid):
+
+
+# Help route : GET : http://localhost:3203/help
+@app.route("/help", methods=['GET'])
+def help():
+    return make_response(render_template('help.html', body_text='This is my HTML help documentation for User service'), 200)
+
+
+# Get bookings by user id : GET : http://localhost:3203/bookings_by_user_id/garret_heaton
+@app.route("/bookings_by_user_id/<userid>", methods=['GET'])
+def bookings_by_user_id(userid):
     for user in users:
         if str(user["id"]) == str(userid):
             bookings = requests.get('http://localhost:3201/bookings/' + userid)
             if bookings.status_code == 200:
                 bookings = bookings.json()
                 return make_response(bookings, 200)
-            return make_response(jsonify({"Error": "User don't have booking"}), 400)
-    return make_response(jsonify({"Error": "user not exist"}), 400)
-@app.route("/bookingsdetailbyuserid/<userid>", methods=['GET'])
-def bookingsdetailbyuserid(userid):
+            return make_response(jsonify({"Error": "User don't have booking"}), 401)
+    return make_response(jsonify({"Error": "User not exist"}), 402)
+
+
+# Get bookings withs details by user id : GET : http://localhost:3203/bookings_details_by_user_id/garret_heaton
+@app.route("/bookings_details_by_user_id/<userid>", methods=['GET'])
+def bookings_details_by_user_id(userid):
     for user in users:
         if str(user["id"]) == str(userid):
             bookings = requests.get('http://localhost:3201/bookings/' + userid)
@@ -33,11 +42,11 @@ def bookingsdetailbyuserid(userid):
                 for date in bookings["dates"]:
                     movieDetail = []
                     for movie in date["movies"]:
-                        movieDetail.append(requests.get('http://localhost:3230/movies/' + movie).json())
+                        movieDetail.append(requests.get('http://localhost:3200/movies/' + movie).json())
                     date["movies"] = movieDetail
                 return make_response(bookings, 200)
-            return make_response(jsonify({"Error": "User don't have booking"}), 400)
-    return make_response(jsonify({"Error": "User not exist"}), 400)
+            return make_response(jsonify({"Error": "User don't have booking"}), 401)
+    return make_response(jsonify({"Error": "User not exist"}), 402)
 
 
 if __name__ == "__main__":
